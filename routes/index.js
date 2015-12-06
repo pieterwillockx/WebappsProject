1ae1eca314fc9cd495934dd3aa0e3eea5c276941
Pieter Willockx <pieterwillockx2@gmail.com>
2015-12-06 04:55:49 +0100

    title van index pagina veranderd.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user, title: "Express" });
+    res.render('index', { user : req.user, title: "Sportzorg.be | Bart Willockx" });
 });
 
 module.exports = router;
